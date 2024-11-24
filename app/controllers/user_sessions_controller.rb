fbbc8cab43406d8d043d3774145fea4e183f9159
Aya141814 <katsura0084@gmail.com>
2024-11-24 21:26:45 +0900

    lintエラー修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
     if @user
       redirect_to root_path, success: "ログインしました"
     else
-      flash.now[:danger] ="ログインに失敗しました" 
+      flash.now[:danger] ="ログインに失敗しました"
       render :new, status: :unprocessable_entity
     end
   end
