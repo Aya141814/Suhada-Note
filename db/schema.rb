4490e01dbff690fbf3b99ec81cef711dae0d0f89
Aya141814 <katsura0084@gmail.com>
2024-12-15 20:36:48 +0900

    タイトルの表示

--- db/schema.rb
+++ db/schema.rb
@@ -44,10 +44,10 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_09_120621) do
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
