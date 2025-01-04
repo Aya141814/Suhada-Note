974e359fbdf91c86e410c4f59e2dfb37c530ef7b
Aya141814 <katsura0084@gmail.com>
2025-01-04 13:15:21 +0900

    cheerボタンのajax化

--- app/controllers/cheers_controller.rb
+++ app/controllers/cheers_controller.rb
@@ -1,13 +1,11 @@
 class CheersController < ApplicationController
   def create
-    board = Board.find(params[:board_id])
-    current_user.cheer(board)
-    redirect_to boards_path, success: t(".success")
+    @board = Board.find(params[:board_id])
+    current_user.cheer(@board)
   end
 
   def destroy
-    board = current_user.cheers.find(params[:id]).board
-    current_user.uncheer(board)
-    redirect_to boards_path, success: t(".success"), status: :see_other
+    @board = current_user.cheers.find(params[:id]).board
+    current_user.uncheer(@board)
   end
 end
