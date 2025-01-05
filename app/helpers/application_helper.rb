e21e7cd7209210da400d395d56074b23b030fa2f
Aya141814 <katsura0084@gmail.com>
2025-01-06 00:15:30 +0900

    lint修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
     keywords: "スキンケア,美容,健康,継続,応援,共有",
     canonical: request.original_url,
     separator: "|",
-    og:{
+    og: {
       site_name: "Skin Care Cheer",  # 文字列として指定
       title: "スキンケアの記録、継続を目指すサービス",  # 文字列として指定
       description: "Skin Care Cheerでは、スキンケアの記録を残し、ユーザー同士で応援し合うことでスキンケアの継続を習慣化を達成するためのサービスです",  # 文字列として指定
@@ -28,6 +28,5 @@ module ApplicationHelper
       image: image_url("ogp.png")
     }
   }
-end
-
+  end
 end
