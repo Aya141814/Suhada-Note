e956e6603ef3e34c2e8c6f1628b693dae20b7227
Aya141814 <katsura0084@gmail.com>
2025-03-21 17:51:59 +0900

    lint修正

--- app/models/board.rb
+++ app/models/board.rb
@@ -12,7 +12,7 @@ class Board < ApplicationRecord
   serialize :skincare_items, coder: YAML
   # 肌トラブルを配列として扱う
   serialize :skin_troubles, coder: YAML
-  
+
   after_initialize :set_default_skincare_items, if: :new_record?
   after_initialize :set_default_skin_troubles, if: :new_record?
 
@@ -28,7 +28,7 @@ class Board < ApplicationRecord
   def set_default_skincare_items
     self.skincare_items ||= []
   end
-  
+
   def set_default_skin_troubles
     self.skin_troubles ||= []
   end
