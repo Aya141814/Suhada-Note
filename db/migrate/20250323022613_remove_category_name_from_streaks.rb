423b1f663b484a1a09ba079db7e666ae46dccd51
Aya141814 <katsura0084@gmail.com>
2025-03-25 11:32:27 +0900

    lint修正

--- db/migrate/20250323022613_remove_category_name_from_streaks.rb
+++ db/migrate/20250323022613_remove_category_name_from_streaks.rb
@@ -1,7 +1,7 @@
 class RemoveCategoryNameFromStreaks < ActiveRecord::Migration[7.2]
   def change
     # インデックスを先に削除
-    remove_index :streaks, [:user_id, :category_name], if_exists: true
+    remove_index :streaks, [ :user_id, :category_name ], if_exists: true
     # カラムを削除
     remove_column :streaks, :category_name
   end
