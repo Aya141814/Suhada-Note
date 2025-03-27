11fce687ed010f7e15569a761fa8bd6fdd973db7
Aya141814 <katsura0084@gmail.com>
2025-03-27 13:46:12 +0900

    見た目色付け、エラーメッセージ編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :require_login
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  add_flash_types :success, :danger
+  add_flash_types :info, :success, :warning, :error
 
   private
 
   def not_authenticated
-    redirect_to login_path, daaanger: t("defaults.flash_message.require_login")
+    redirect_to login_path, error: t("defaults.flash_message.require_login")
   end
 end
