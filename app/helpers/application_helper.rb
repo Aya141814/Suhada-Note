d98ba5b016afe45580ee4505d97da0c2ac27e7b1
Aya141814 <katsura0084@gmail.com>
2025-03-28 16:36:18 +0900

    faviconの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,22 @@
 module ApplicationHelper
   def page_title(title = "")
-    base_title = "Skin Care Cheer"
+    base_title = "Suhada Note"
     title.present? ? "#{title} | #{base_title}" : base_title
   end
   def default_meta_tags
   {
-    site: "Skin Care Cheer",
+    site: "Suhada Note",
     title: "スキンケアの記録、継続を目指すサービス",
     reverse: true,
     charset: "utf-8",
-    description: "Skin Care Cheerでは、スキンケアの記録を残し、ユーザー同士で応援し合うことでスキンケアの継続を習慣化を達成するためのサービスです。",
+    description: "Suhada Noteでは、スキンケアの記録を残し、ユーザー同士で応援し合うことでスキンケアの継続を習慣化を達成するためのサービスです。",
     keywords: "スキンケア,美容,健康,継続,応援,共有",
     canonical: request.original_url,
     separator: "|",
     og: {
-      site_name: "Skin Care Cheer",  # 文字列として指定
+      site_name: "Suhada Note",  # 文字列として指定
       title: "スキンケアの記録、継続を目指すサービス",  # 文字列として指定
-      description: "Skin Care Cheerでは、スキンケアの記録を残し、ユーザー同士で応援し合うことでスキンケアの継続を習慣化を達成するためのサービスです",  # 文字列として指定
+      description: "Suhada Noteでは、スキンケアの記録を残し、ユーザー同士で応援し合うことでスキンケアの継続を習慣化を達成するためのサービスです",  # 文字列として指定
       type: "website",
       url: request.original_url,
       image: image_url("ogp.png"),
