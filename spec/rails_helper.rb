d1555e2b99ea7b45f4bbd0ab5fb9f0f6555f1abf
Aya141814 <katsura0084@gmail.com>
2025-05-08 22:40:03 +0900

    修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,11 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # return unless Rails.env.test?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+# テスト環境ではファイルアップロードをローカルに設定
+CarrierWave.configure do |config|
+  config.storage = :file
+  config.enable_processing = false
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
