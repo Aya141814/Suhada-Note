70aff2393a644c39fc19e673c335ce223a91240c
Aya141814 <katsura0084@gmail.com>
2025-05-09 23:53:26 +0900

    topページを実装

--- config/application.rb
+++ config/application.rb
@@ -13,12 +13,12 @@ module Myapp
 
     # Zeitwerkの設定
     config.autoloader = :zeitwerk
-    
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-    
+
     if Rails.env.test?
       # テスト環境での自動読み込みの設定
       config.add_autoload_paths_to_load_path = true
